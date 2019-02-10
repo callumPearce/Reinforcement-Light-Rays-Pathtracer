e084ba71c806ff7a63c124f1571fc9b32cc2637f
callumPearce <cp15571@gmail.com>
2019-02-10 15:30:28 +0000

    object loader complete without correct translation

--- Source/main.cpp
+++ Source/main.cpp
@@ -112,7 +112,7 @@ int main (int argc, char* argv[]) {
     // Load the shapes within the scene
     vector<Triangle> triangles;
     get_cornell_shapes(triangles);
-    // load_scene("Models/box.obj", triangles);
+    // load_scene("Models/cube.obj", triangles);
 
     // Convert all shapes into a unified list of pointers to them
     vector<Shape *> shapes;
--- Source/objects/object_importer.cpp
+++ Source/objects/object_importer.cpp
@@ -57,20 +57,20 @@ bool load_scene(const char* path, vector<Triangle>& triangles){
                             split_string(temp_args, args[i], "/");
                             indices.push_back(stoi(temp_args[0], nullptr, 10));
                         }
-                        for (int i = 0; i < indices.size()-2; i++){
-                            int x = indices[i];
-                            int y = indices[i+1];
-                            int z = indices[i+2];
-                            vertex_indices.push_back(vec3(x,y,z));
+                        int start_index = indices[0];
+                        for (int i = 1; i < indices.size()-1; i++){
+                            int y = indices[i];
+                            int z = indices[i+1];
+                            vertex_indices.push_back(vec3(start_index,y,z));
                         }
                     } 
-                    // Otherwise just convert the three integer indices
+                    // Otherwise just convert the three integer indices (via Fan Triangulation)
                     else{
-                        for (int i = 0; i < args.size()-2; i++){
-                            int x = stoi(args[i], nullptr, 10);
-                            int y = stoi(args[i+1], nullptr, 10);
-                            int z = stoi(args[i+2], nullptr, 10);
-                            vertex_indices.push_back(vec3(x,y,z));
+                        int start_vertex = stoi(args[0], nullptr, 10);
+                        for (int i = 1; i < args.size()-1; i++){
+                            int y = stoi(args[i], nullptr, 10);
+                            int z = stoi(args[i+1], nullptr, 10);
+                            vertex_indices.push_back(vec3(start_vertex,y,z));
                         }
                     }
                 }
@@ -83,8 +83,6 @@ bool load_scene(const char* path, vector<Triangle>& triangles){
 
 // Convert the temporary stored data into triangles for rendering
 void build_triangles(vector<Triangle>& triangles, vector<vec3>& vertex_indices, vector<vec3>& temp_vertices){
-
-    cout << vertex_indices.size() << endl;
 
     // Find the max and min vertex position of each dimension
     float max_pos[3] = {0.f};
@@ -109,9 +107,13 @@ void build_triangles(vector<Triangle>& triangles, vector<vec3>& vertex_indices, 
         }
     }
 
-    // Scale down/up to fit [1,-1] on max dimension
+    // Scale down/up to fit [-1,1] on max dimension
     float scale = 2 / max_difference;
     
+    // Translate so that a vertex can be at min [-1,-1,-1]
+    // float dist_x = -1.f;//(min_pos[0] * scale);
+    // float dist_y = -1.f;//(min_pos[1] * scale);
+    // float dist_z = -1.f;//(min_pos[2] * scale);
 
     // For each vertex
     for (int i = 0; i < vertex_indices.size(); i++){
@@ -120,12 +122,22 @@ void build_triangles(vector<Triangle>& triangles, vector<vec3>& vertex_indices, 
         vec4 v1 = vec4(temp_vertices[(int)vertex_indices[i].x - 1],1.f)*scale;
         vec4 v2 = vec4(temp_vertices[(int)vertex_indices[i].y - 1],1.f)*scale;
         vec4 v3 = vec4(temp_vertices[(int)vertex_indices[i].z - 1],1.f)*scale;
+        // v1 += vec4(dist_x, dist_y, dist_z, 0.f);
+        // v2 += vec4(dist_x, dist_y, dist_z, 0.f);
+        // v3 += vec4(dist_x, dist_y, dist_z, 0.f);
+        
+        vec4 rotation = vec4(-1.f, -1.f, 1.f, 1.f);
+
+        v1 *= rotation;
+        v2 *= rotation;
+        v3 *= rotation;
+        
         v1.w = 1.f;
         v2.w = 1.f;
         v3.w = 1.f;
 
         // Construct a Triangle with white material (TODO: Implement materials properly)
-        Triangle triangle = Triangle(v1, v2, v3, Material(vec3(0.75f, 0.75f, 0.75f)));
+        Triangle triangle = Triangle(v1, v3, v2, Material(vec3(0.75f, 0.75f, 0.75f)));
 
         // Compute and set the normal
         triangle.compute_and_set_normal();
