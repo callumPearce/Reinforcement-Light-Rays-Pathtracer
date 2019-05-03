5ecdcb54aa78edc451bb0dc6eaf6c8851bea776d
callumPearce <cp15571@gmail.com>
2019-05-03 19:03:49 +0100

    max 1spp images

--- GPU_Rendering_Engine/Source/deep_learning/pre_trained_pathtracer.cu
+++ GPU_Rendering_Engine/Source/deep_learning/pre_trained_pathtracer.cu
@@ -42,7 +42,7 @@ PretrainedPathtracer::PretrainedPathtracer(
     //////////////////////////////////////////////////////////////
     /*             Load in the Parameter Values                 */
     //////////////////////////////////////////////////////////////
-    std::string fname = "/home/calst/Documents/year4/thesis/monte_carlo_raytracer/Radiance_Map_Data/cornell_no_decay.model";
+    std::string fname = "/home/calst/Documents/year4/thesis/monte_carlo_raytracer/Radiance_Map_Data/archway_12_12.model";
     if (file_exists(fname)){
         dynet::TextFileLoader loader(fname);
         loader.populate(model);
